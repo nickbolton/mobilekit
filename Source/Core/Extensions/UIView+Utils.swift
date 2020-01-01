e213c0bb2a5c8c6d2f9189f308adc7b37b60b444
nickbolton <bolton.nick@gmail.com>
2020-01-01 08:01:07 -0600

    added rounded corner utils

--- Source/Core/Extensions/UIView+Utils.swift
+++ Source/Core/Extensions/UIView+Utils.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 extension UIView {
-    
+  
   func walkHierarchyAndApplyTheme() {
     walkViewHierarchy { v -> Bool in
       if let themeable = v as? Themeable {
@@ -18,7 +18,7 @@ extension UIView {
       return (v as? InternalThemeable) != nil
     }
   }
-
+  
   public func convertCenter(to: UIView!) -> CGPoint {
     let center = CGPoint(x: bounds.midX, y: bounds.midY)
     return convert(center, to: to)
@@ -30,5 +30,48 @@ extension UIView {
       if handler(v) { continue }
       v.walkViewHierarchy(handler)
     }
+  }
+  
+  public func removeCornerRounding() {
+    layer.mask = nil
+  }
+  
+  public func roundLeftCorners(radius: CGFloat, rect: CGRect? = nil) {
+    let corners = UIRectCorner.topLeft.union(.bottomLeft)
+    let path = UIBezierPath(roundedRect: rect ?? bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+    let mask = CAShapeLayer()
+    mask.path = path.cgPath
+    layer.mask = mask
+  }
+  
+  public func roundRightCorners(radius: CGFloat, rect: CGRect? = nil) {
+    let corners = UIRectCorner.topRight.union(.bottomRight)
+    let path = UIBezierPath(roundedRect: rect ?? bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+    let mask = CAShapeLayer()
+    mask.path = path.cgPath
+    layer.mask = mask
+  }
+  
+  public func roundTopCorners(radius: CGFloat, rect: CGRect? = nil) {
+    let corners = UIRectCorner.topRight.union(.topLeft)
+    let path = UIBezierPath(roundedRect: rect ?? bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+    let mask = CAShapeLayer()
+    mask.path = path.cgPath
+    layer.mask = mask
+  }
+  
+  public func roundBottomCorners(radius: CGFloat, rect: CGRect? = nil) {
+    let corners = UIRectCorner.bottomRight.union(.bottomLeft)
+    let path = UIBezierPath(roundedRect: rect ?? bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+    let mask = CAShapeLayer()
+    mask.path = path.cgPath
+    layer.mask = mask
+  }
+  
+  public func roundCorners(corners: UIRectCorner, radius: CGFloat, rect: CGRect? = nil) {
+    let path = UIBezierPath(roundedRect: rect ?? bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+    let mask = CAShapeLayer()
+    mask.path = path.cgPath
+    layer.mask = mask
   }
 }
