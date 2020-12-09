418133ea3321bf0c7112dbd8f4b8408e050ab2c6
nickbolton <bolton.nick@gmail.com>
2020-12-09 07:25:40 -0600

    added dump available fonts util

---
+++ Source/Core/Extensions/UIFont+Utils.swift
@@ -0,0 +1,18 @@
+//
+//  UIFont+Utils.swift
+//  MobileKit
+//
+//  Created by Nick Bolton on 12/9/20.
+//
+
+import UIKit
+
+extension UIFont {
+
+  func dumpAvailableFonts() {
+    for family in UIFont.familyNames {
+      let names = UIFont.fontNames(forFamilyName: family)
+      print("\(family) \(names)")
+    }
+  }
+}
